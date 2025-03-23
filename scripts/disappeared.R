a86b4231f65d92ec49f67759c5650d7508a9b513
tomdavidoff <thomas.davidoff@sauder.ubc.ca>
2025-03-23 14:33:32 -0700

    from sat pm

--- scripts/disappeared.R
+++ scripts/disappeared.R
@@ -27,7 +27,7 @@ dI <- merge(dI,da,by.x="roll_number",by.y="rollNumber")
 
 # close connection
 dbDisconnect(con)
-df <- data.table(dI) 
+df <- unique(data.table(dI))
 df[,streetNumber16:=as.numeric(streetNumber)]
 df[,fullStreet:=paste(streetNumber,streetDirectionPrefix,streetName,streetType,streetDirectionSuffix)]
 df[,streetNoNumber:=paste(streetDirectionPrefix,streetName,streetType,streetDirectionSuffix)]
@@ -43,18 +43,22 @@ print(df[1:10,.(roll_number,rollStart,rollEnd)])
 
 
 # 2024/5 disappeared lots -- what is the current use?
-d24 <- fread("~/docs/data/bca/data_advice_REVD24_20240331/bca_folio_descriptions_20240331_REVD24.csv",select=c("FOLIO_ID","ROLL_NUMBER","ACTUAL_USE_DESCRIPTION","JURISDICTION_CODE"))
+d24 <- fread("~/docs/data/bca/data_advice_REVD24_20240331/bca_folio_descriptions_20240331_REVD24.csv",select=c("FOLIO_ID","ROLL_NUMBER","ACTUAL_USE_DESCRIPTION","JURISDICTION_CODE"),colClasses=c(ROLL_NUMBER="character"))
 d24 <- d24[JURISDICTION_CODE==200]
 #dI <- dI[Zoning=="R1-1" & Jurisdiction==200]
 d24[,nchar:=nchar(as.character(ROLL_NUMBER))]
 d24[,splitspot:=nchar-4]
 d24[,rollStart:=substr(ROLL_NUMBER,1,splitspot)]
 d24[,rollEnd24:=substr(ROLL_NUMBER,splitspot+1,nchar)]
+
+dI24 <- fread("~/docs/data/bca/Residential_inventory_202401/20240101_AA09_Residential_Inventory_Extract.txt",select=c("Jurisdiction","Roll_Number","MB_Year_Built","Zoning"),colClasses=c(Roll_Number="character"))
+dI24 <- dI24[Jurisdiction==200]
+d24 <- merge(d24,dI24,by.x="ROLL_NUMBER",by.y="Roll_Number")
 d24[,ROLL_NUMBER:=NULL]
 
 da <- fread("~/docs/data/bca/data_advice_REVD24_20240331/bca_folio_addresses_20240331_REVD24.csv",select=c("FOLIO_ID","STREET_NUMBER","STREET_DIRECTION_PREFIX","STREET_NAME","STREET_TYPE","STREET_DIRECTION_SUFFIX","POSTAL_CODE"),colClasses=c(FOLIO_ID="character"))
 da[,fullStreet24:=paste(STREET_NUMBER,STREET_DIRECTION_PREFIX,STREET_NAME,STREET_TYPE,STREET_DIRECTION_SUFFIX)]
-da[,streetNoNumber:=paste(STREET_DIRECTION_PREFIX,STREET_NAME,STREET_TYPE,STREET_DIRECTION_SUFFIX)]
+da[,streetNoNumber24:=paste(STREET_DIRECTION_PREFIX,STREET_NAME,STREET_TYPE,STREET_DIRECTION_SUFFIX)]
 da[,streetNumber24:=as.numeric(STREET_NUMBER)]
 
 # GET YEAR BUILT!!
@@ -62,8 +66,62 @@ da[,streetNumber24:=as.numeric(STREET_NUMBER)]
 d24 <- merge(d24,da,by="FOLIO_ID")
 
 # merge with 2016
-df <- merge(df,d24,by="rollStart",all.x=TRUE)
+df <- merge(df,d24,by="rollStart",all.x=TRUE) # note: merge on interstitial address almost never happens
+dfx <- df[is.na(rollEnd24),]
+df <- df[!is.na(rollEnd24),]
+dfx <- merge(dfx,d24,by.x="fullStreet",by.y="fullStreet24")
+
+df <- df[,.(FOLIO_ID,ROLL_NUMBER,MB_year_built,MB_Year_Built,actualUseDescription,ACTUAL_USE_DESCRIPTION)]
+dfx <- dfx[,.(FOLIO_ID,ROLL_NUMBER,MB_year_built,MB_Year_Built,actualUseDescription,ACTUAL_USE_DESCRIPTION)]
+df <- rbind(df,dfx)
+
+
+if (2>3) {
+df[,streetNumber16:=as.numeric(streetNumber16)]
+df[,streetNumber24:=as.numeric(streetNumber24)]
+df[,evenStreet:=streetNumber16%%2==0]
+df[,evenStreet24:=streetNumber24%%2==0]
 
 # find street numbers that are missing in 2024
-dfk <- df[is.na(streetNumber24),]
-print(table(dfk[,fullStreet %in% d24[i,fullStreet24]]))
+dfk <- df[is.na(streetNumber24 | is.na(streetNumber)),]
+print(table(dfk[,fullStreet %in% d24[fullStreet24]])) # always false
+
+dfk[,lowestHigherNeighbour:=0]
+outcomes <- data.table(type=character())
+for (i in 1:nrow(dfk)) {
+    # get upper and lower limits of addresses that aren't this particular one
+    if (is.na(dfk[i,streetNumber24])) {
+        print(i)
+        thisStreet<- dfk[i,streetNoNumber][1]
+        thisNumber<- dfk[i,streetNumber16][1]
+        thisEven<- dfk[i,evenStreet][1]
+        thisAddress <- dfk[i,fullStreet][1]
+        print(dfk[i,fullStreet])
+        consideration <- df[streetNoNumber==thisStreet & evenStreet==thisEven]
+        print(consideration[,.N])
+        consideration[,lowestHigherNeighbour:=min(999999*(streetNumber16<=thisNumber)+(streetNumber16),na.rm=TRUE)]
+        consideration[,highestLowerNeighbour:=max((streetNumber16<=thisNumber)*streetNumber16,na.rm=TRUE)]
+        print(c(thisStreet,thisNumber,consideration[1,lowestHigherNeighbour],consideration[1,highestLowerNeighbour]))
+        consideration[,replacement:= streetNumber24<lowestHigherNeighbour & streetNumber24>highestLowerNeighbour]
+        print(consideration[replacement==TRUE,.(streetNumber24)])
+        print(df[fullStreet24==thisAddress,.(fullStreet24,streetNoNumber24)])
+        outcome <- "neither"
+        if (sum(consideration[,replacement]>0,na.rm=TRUE)>0) {
+            if (sum(df[,fullStreet24==thisAddress],na.rm=TRUE)>0) {
+                outcome  <- "both"
+            } 
+            else {
+                    outcome <- "replacement"
+                }
+        }
+        else {
+            if (sum(df[,fullStreet24==thisAddress],na.rm=TRUE)>0) {
+                outcome <- "address"
+            }
+        }
+    outcomes <- rbind(outcomes,data.table(type=outcome))
+    }
+}
+
+print(table(outcomes[,type]))
+}
