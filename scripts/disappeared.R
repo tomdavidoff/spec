e18c9f1048fb36d3d3599c5492a8a9513c2330b0
tomdavidoff <tom.davidoff@gmail.com>
2025-03-26 18:15:07 -0700

    Mar 26

--- scripts/disappeared.R
+++ scripts/disappeared.R
@@ -1,7 +1,10 @@
 #disappeared.R
 # Single family lots that aren't there any more but are duplexes
+# Generally get 2024 status of 2016 lots; confine to 30' or 50' by 120 and have laneway
 # Tom Davidoff
 # March 17, 2025
+
+TOLERANCE <- 1.5 # 2 foot tolerance around tolerated lot sizes
 
 library(data.table)
 library(RSQLite)
@@ -13,6 +16,12 @@ library(RSQLite)
 # open connection first
 con <- dbConnect(RSQLite::SQLite(), "~/docs/data/bca/REVD16_and_inventory_extracts.sqlite3")
 dI <- dbGetQuery(con, "SELECT  roll_number, zoning, land_width, land_depth, MB_year_built FROM residentialInventory WHERE jurisdiction=='200' AND (zoning=='RS1' OR zoning=='RS2' OR zoning=='RS3' OR zoning=='RS3A' OR zoning=='RS4' OR zoning=='RS5' OR zoning=='RS6' OR zoning=='RS7' )")
+dI <- data.table(dI)
+print(summary(dI[,as.numeric(land_depth)]))
+dI[,thirty:=land_width<=33+TOLERANCE & land_width>=33-TOLERANCE & land_depth<=122+2*TOLERANCE & land_depth>=122-2*TOLERANCE]
+dI[,fifty:=land_width<=50+TOLERANCE & land_width>=50-TOLERANCE & land_depth<=122+2*TOLERANCE & land_depth>=122-2*TOLERANCE]
+dI <- dI[thirty==TRUE | fifty==TRUE]
+
 
 da <- dbGetQuery(con, "SELECT folioID,streetNumber,streetDirectionPrefix,streetName,streetType,streetDirectionSuffix,postalCode,city FROM address ")
 
@@ -24,6 +33,30 @@ da <- merge(da,dfd,by="folioID")
 
 
 dI <- merge(dI,da,by.x="roll_number",by.y="rollNumber")
+#print(table(dI[,streetType]))
+#AVE   CRES DIVERS     DR    HWY     PL     RD     ST 
+# print(table(dI[,streetDirectionPrefix])) empty
+#print(table(dI[,streetDirectionSuffix]))        E    N   SE   SW    W 
+
+dI[,streetTypeForLaneway:=ifelse(streetType=="AVE","AV",ifelse(streetType=="CRES","CRESCENT",ifelse(streetType=="DR","DRIVE",ifelse(streetType=="PL","PLACE",ifelse(streetType=="RD","ROAD",streetType)))))]
+dI[,addressForLaneway:=trimws(paste(streetNumber,streetDirectionSuffix,streetName,streetTypeForLaneway))]
+dI[,addressForLaneway:=gsub("  "," ",addressForLaneway)]
+
+dLaneOk <- fread("data/derived/lanewaySpatial.csv")
+dLaneOk[,addressForLaneway:=trimws(paste(streetNumber,streetName))]
+dLaneOk[,streetNumber:=NULL]
+dLaneOk[,streetName:=NULL]
+dI <- merge(dI,dLaneOk,by="addressForLaneway",all.x=TRUE,all.y=TRUE)
+# deal with NA laneok approximation
+dI[,madeupLaneok:=is.na(laneok)]
+dI[,roundNumber:=floor(as.numeric(streetNumber)/100)]
+dI[,meanLaneok:=mean(laneok,na.rm=TRUE),by=roundNumber]
+dI[is.na(laneok) & meanLaneok==0,laneok:=0]
+print(table(dI[,.(is.na(laneok),streetType)]))
+dI[is.na(laneok) & meanLaneok==1,laneok:=1]
+print(table(dI[,.(is.na(laneok),streetType)]))
+
+dfdf
 
 # close connection
 dbDisconnect(con)
---
+++ scripts/main.R
@@ -0,0 +1,10 @@
+# main.R
+# R to analyze spec/custom choice
+# Tom Davidoff
+
+library(data.table)
+
+
+
+# Main regression
+# Dummy for spec vs custom on property type dummies and share 
